f4a9c6c8e6708e78474f1176c26f28034105f071
Sunil Gupta <sunilg@infinite.com>
2025-12-18 12:08:15 +0530

    Code Bloom Auth project started

---
+++ codebloom-auth/src/main/java/com/java/codebloom/auth/CodebloomAuthApplication.java
@@ -0,0 +1,13 @@
+package com.java.codebloom.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodebloomAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodebloomAuthApplication.class, args);
+	}
+
+}
---
+++ codebloom-auth/src/test/java/com/java/codebloom/auth/CodebloomAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.codebloom.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodebloomAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
